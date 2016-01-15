4f8e84e4751b5c59f260506da41459af4d9307d3
shanghai-Jerry <cjyou20110920sspu@163.com>
2016-01-15 17:24:04 +0800

    +读取信息模块

---
+++ src/main/scala/analysis/TimeUtil.scala
@@ -0,0 +1,20 @@
+package analysis
+
+/**
+  * Created by C.J.YOU on 2016/1/13.
+  */
+ object TimeUtil {
+  def GetTime(timeStamp: String): String = {
+    val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val bigInt: BigInteger = new BigInteger(timeStamp)
+    val date: String = sdf.format(bigInt)
+    date
+  }
+
+  def GetDate(timeStamp: String): String = {
+    val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH")
+    val bigInt: BigInteger = new BigInteger(timeStamp)
+    val date: String = sdf.format(bigInt)
+    date
+  }
+}
