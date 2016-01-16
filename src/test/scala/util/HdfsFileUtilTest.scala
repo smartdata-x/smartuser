7875fa2bb898f4ad410010c8744f1f6ef14376fc
shanghai-Jerry <cjyou20110920sspu@163.com>
2016-01-16 15:11:53 +0800

    + add unit method  test for HdfsFileUtil

--- src/test/scala/util/HdfsFileUtilTest.scala
+++ src/test/scala/util/HdfsFileUtilTest.scala
@@ -1,5 +1,7 @@
 package util
 
+import analysis.DataAnalysis
+import analysis.analysis.DataAnalysis
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.collection.mutable
@@ -12,6 +14,24 @@ class HdfsFileUtilTest extends FlatSpec with Matchers {
   it should "" in {
 
   }
+  "saveStockCodes and MergeList " should "work" in{
+    HdfsFileUtil.setHdfsUri("hdfs://server:9000")
+    HdfsFileUtil.setRootDir("smartuser")
+    val currentPath = HdfsFileUtil.mkDir(HdfsFileUtil.getRootDir+"days")
+    HdfsFileUtil.mkFile(currentPath+"file1")
+    var list = new mutable.MutableList[String]
+    println("currentPath:"+currentPath)
+    list.+=("000001")
+    list.+=("000002")
+    val list_2 = new mutable.MutableList[String]
+    list_2.+=("000001")
+    list_2.+=("000002")
+    list_2.+=("000003")
+    list_2.+=("000005")
+    list = DataAnalysis.MergeList(list,list_2)
+    // HdfsFileUtil.writeStockCode(currentPath +"file1",list)
+    HdfsFileUtil.saveStockCodes(currentPath +"file1",list)
+  }
 
   "read and write method" should "work" in {
     HdfsFileUtil.setHdfsUri("hdfs://server:9000")
@@ -21,7 +41,7 @@ class HdfsFileUtilTest extends FlatSpec with Matchers {
     val list = new mutable.MutableList[String]
     list.+=("000001")
     list.+=("000002")
-    HdfsFileUtil.writeToFile(currentPath +"file1",list)
+    HdfsFileUtil.writeStockCode(currentPath +"file1",list)
 
 
   }
