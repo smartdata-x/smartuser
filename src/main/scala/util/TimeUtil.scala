2d52be12c3f5021153b5509ec84f052f4f94d904
ysrotciv <ysrotciv@gmail.com>
2016-01-16 11:12:58 +0800

    refactor

--- src/main/scala/util/TimeUtil.scala
+++ src/main/scala/util/TimeUtil.scala
@@ -7,15 +7,16 @@ import java.text.SimpleDateFormat
   * Created by C.J.YOU on 2016/1/13.
   */
  object TimeUtil {
+
   def GetTime(timeStamp: String): String = {
-    val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss")
     val bigInt: BigInteger = new BigInteger(timeStamp)
     val date: String = sdf.format(bigInt)
     date
   }
 
   def GetDate(timeStamp: String): String = {
-    val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH")
+    val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd_HH")
     val bigInt: BigInteger = new BigInteger(timeStamp)
     val date: String = sdf.format(bigInt)
     date
