2335dfb5009881af30ddbe863d3e65614366d200
ysrotciv <ysrotciv@gmail.com>
2016-01-17 22:46:48 +0800

    delete useless import

--- src/main/scala/scheduler/Scheduler.scala
+++ src/main/scala/scheduler/Scheduler.scala
@@ -4,7 +4,6 @@ import java.io.{BufferedWriter, FileWriter, File}
 import java.text.SimpleDateFormat
 import java.util.Calendar
 
-import analysis.TableHbase
 import dispatch._,Defaults._
 import log.SULogger
 import org.apache.commons.io.FileUtils
