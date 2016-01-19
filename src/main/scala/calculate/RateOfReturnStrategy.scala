058c7ab2b32e215299a9269e3c5fdc98fe66ede1
kerry <kerry@miglab.com>
2016-01-19 08:41:44 +0800

    +新增用户对象

--- src/main/scala/calculate/RateOfReturnStrategy.scala
+++ src/main/scala/calculate/RateOfReturnStrategy.scala
@@ -2,12 +2,11 @@ package calculate
 
 import stock.Stock
 import stock.RateOfReturn
-import config.StrategyConig
 
 
 /**
   * Created by yangshuai on 2016/1/15
-  * 回报率计算策略
+  * 单支股票回报率计算策略
   */
 trait RateOfReturnStrategy {
 
---
+++ src/main/scala/stock/UserStock.scala
@@ -0,0 +1,29 @@
+package stock
+import scala.collection.mutable
+
+/**
+  * Created by kerry on 16/1/17.
+  */
+class UserStock {
+  private var _uid: String = new String
+  private val _hash: mutable.HashMap[String, RateOfReturn] = new mutable.HashMap[String, RateOfReturn]
+  private val _currentRate  = 0.0
+
+  def this(uid : String) = {
+    this()
+    this._uid = uid
+  }
+
+  def setStock(stock: RateOfReturn): Unit ={
+    this._hash(stock.getCode()) = stock
+  }
+
+  def setStockes(stockes:mutable.HashMap[String,String]): Unit ={
+    this._hash.++ (stockes)
+  }
+
+  def getCurrentRate():Double = {
+    return this._currentRate
+  }
+
+}
