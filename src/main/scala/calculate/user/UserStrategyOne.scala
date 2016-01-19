d3967a679aa34c733c59ab0ebcaf0d83c1bd7ca7
kerry <kerry@miglab.com>
2016-01-19 15:33:13 +0800

    +加入用户回报率策略

--- src/main/scala/calculate/user/UserStrategyOne.scala
+++ src/main/scala/calculate/user/UserStrategyOne.scala
@@ -1,8 +1,13 @@
 package calculate.user
 
+import stock.UserStock
+
 /**
   * Created by kerry on 16/1/19.
+  * 计算每个用户回报率如果为负数则返回false
   */
-class UserStrategyOne {
-
+class UserStrategyOne extends UserRateOfReturn{
+  override def calculate(u:UserStock):Boolean = {
+    return true
+  }
 }
