bf1c965406fe6394b45f79534fe890c1f6079764
ysrotciv <ysrotciv@gmail.com>
2016-01-19 21:13:31 +0800

    补充方法

--- src/main/scala/stock/RateOfReturn.scala
+++ src/main/scala/stock/RateOfReturn.scala
@@ -4,10 +4,12 @@ package stock
   * Created by kerry on 16/1/18.
   */
 class RateOfReturn {
+
   private var _code:String = ""//股票代碼
   private var _name:String = ""//股票名称
   private var _current_rate:Double = 0.000 //回报率
   private var _date: String = "" //回报率的时间段
+
   def this(code :String,name : String) = {
     this()
     this._code = code
@@ -19,21 +21,23 @@ class RateOfReturn {
     this._current_rate = current_rate
   }
 
-  def getCode():String = {
-    return this._code
+  def getCode:String = {
+    this._code
   }
 
-  def getName():String = {
-    return this._name
+  def getName:String = {
+    this._name
   }
 
-  def getRateOfReturn():Double ={
-    return this._current_rate
+  def getRateOfReturn:Double ={
+    this._current_rate
   }
 
-  def getRateOfReturnDate():String = {
-    return this._date
+  def getRateOfReturnDate:String = {
+    this._date
   }
 
-
+  def setRateOfReturnDate(date: String): Unit = {
+    this._date = date
+  }
 }
