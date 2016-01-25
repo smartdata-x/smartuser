54c72bf886a4d76ae447d625dd9b5b984ca9ba84
ysrotciv <ysrotciv@gmail.com>
2016-01-25 15:33:15 +0800

    aa

--- src/main/scala/config/RegExpConfig.scala
+++
@@ -1,9 +0,0 @@
-package config
-
-/**
-  * Created by yangshuai on 2016/1/22.
-  */
-object RegExpConfig {
-
-  val FLOAT = "[-+]?[0-9]*\\.?[0-9]*"
-}
--- src/main/scala/config/SparkConfig.scala
+++
@@ -1,9 +0,0 @@
-package config
-
-/**
-  * Created by C.J.YOU on 2016/1/17.
-  */
-object SparkConfig {
-  val SPARK_SERIALIZER = "org.apache.spark.serializer.KryoSerializer"
-  val SPARK_KRYOSERIALIZER_BUFFER_MAX = "2000"
-}
