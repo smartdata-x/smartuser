d65384468a4f8e9243d2500e35e9c571a70e9ca3
ysrotciv <ysrotciv@gmail.com>
2016-01-25 13:54:57 +0800

    补充注释

--- src/main/scala/config/FileConfig.scala
+++ src/main/scala/config/FileConfig.scala
@@ -1,16 +1,21 @@
 package config
 
-import util.TimeUtil
-
 /**
   * Created by C.J.YOU on 2016/1/18.
   */
 object FileConfig {
 
   val ROOT_DIR = "/home/smartuser"
+
+  //股票信息
   val STOCK_INFO = ROOT_DIR + "/Stock"
+  //股票回报率
   val RATE_OF_RETURN_STOCK = ROOT_DIR + "/Return_Stock"
+  //用户回报率
   val RATE_OF_RETURN_USER = ROOT_DIR + "/Return_User"
+  //用户所关注的股票
   val USER_INFO = ROOT_DIR + "/User"
-  val Date = TimeUtil.getDay(System.currentTimeMillis().toString)
+  //用户回报率排名
+  val RANK_STOCK = ROOT_DIR + "/Rank_User"
 }
+
