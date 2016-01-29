da865b52b4b938ef6a90ddfa28f8d945533d515b
ysrotciv <ysrotciv@gmail.com>
2016-01-29 20:57:54 +0800

    更新hbase配置信息

--- userinfo/src/main/scala/config/HbaseConfig.scala
+++ userinfo/src/main/scala/config/HbaseConfig.scala
@@ -4,8 +4,8 @@ package config
   * Created by yangshuai on 2016/1/26.
   */
 object HbaseConfig {
-  val HBASE_URL = "hdfs://server:9000"
-  val HBASE_ROOT_DIR = "hdfs://server:9000/hbase"
-  val HBASE_ZOOKEEPER_QUORUM = "server"
+  val HBASE_URL = "hdfs://server3:9000"
+  val HBASE_ROOT_DIR = "hdfs://server3:9000/hbase"
+  val HBASE_ZOOKEEPER_QUORUM = "server3"
   val HBASE_ZOOKEEPER_PROPERTY_CLIENTPORT = "2181"
 }
